11cc91f1182712a5849c76b46f3418c3cb112c96
MJNambiar <marinanambiar@Nikhils-MacBook-Air.local>
2022-07-16 16:48:44 -0500

    Final Commit

--- script.js
+++ script.js
@@ -3,6 +3,22 @@
 
 window.addEventListener("load", function() {
    
+    let form = document.querySelector("form");
+    
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        
+        let pilot = form.elements["pilotName"].value;
+        let copilot = form.elements["copilotName"].value;
+        let fuelLevel = form.elements["fuelLevel"].value;
+        let cargoLevel = form.elements["cargoMass"].value;
+        let list = document.getElementById("faultyItems");
+     
+        formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
+        
+    });
+
+    
    let listedPlanets;
    let listedPlanetsResponse = myFetch();
 
@@ -13,42 +29,6 @@ window.addEventListener("load", function() {
         let chosenPlanet = pickPlanet(listedPlanets);
         addDestinationInfo(document, chosenPlanet.name, chosenPlanet.diameter, chosenPlanet.star, chosenPlanet.distance, chosenPlanet.moons, chosenPlanet.image);
 
-
-        let form = document.querySelector("form");
-        console.log('am I outside submit: ', form);
-        // let pilot = document.getElementById("pilotName").value;
-        // console.log(pilot);
-        // let pilot1 = form.elements["pilotName"];
-        // console.log(pilot1);
-        // console.log(pilot1.value);
-
-        form.addEventListener("submit", function(event) {
-            console.log("am I inside submit function?");
-            //CANT GET INSIDE HERE YET>>>>>
-            let pilot = form.elements["pilotName"].value;
-            let copilot = form.elements["copilotName"].value;
-            let fuelLevel = form.elements["fuelLevel"].value;
-            let cargoLevel = form.elements["cargoMass"].value;
-            let list = document.getElementById("faultyItems");
-            // let pilot = document.getElementById("pilotName").value;
-            // let copilot = document.getElementById("copilotName").value;
-            // let fuelLevel = document.getElementById("fuelLevel").value;
-            // let cargoLevel = document.getElementById("cargoMass").value;
-            // let list = document.getElementById("faultyItems").value;
-            
-            formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
-            // event.preventDefault();
-            console.log("do I get here?");
-   
         });
     });
-});
-
-// window.addEventListener("input", () => {
-//     console.log('hey I am in the input event');  
-// })
-
-// window.addEventListener("click", () => {
-//     console.log('hey I am in the click event');  
-// })
 
--- scriptHelper.js
+++ scriptHelper.js
@@ -31,57 +31,64 @@ function validateInput(testInput) {
 };
 
 
+
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
    
-    // form.addEventListener("submit", function(event) {
+        if (
+            validateInput(pilot) === "Empty" || 
+            validateInput(copilot) === "Empty" || 
+            validateInput(fuelLevel) === "Empty" || 
+            validateInput(cargoLevel) === "Empty") {
 
-        list = document.querySelector("faultyItems");
-        let form = document.querySelector("form");
-       
-        pilot = form.elements["pilotName"].value;
-        copilot = form.elements["copilotName"].value;
-        fuelLevel = form.elements["fuelLevel"].value;
-        cargoLevel = form.elements["cargoMass"].value;
-       
-        if (validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty") {
             alert("All fields are required.");
-            // event.preventDefault();
-        } else if (validateInput(fuelLevel) === "Not a Number" || validateInput(cargoLevel) === "Not a Number") {
-            alert("Field requires a number. Please enter numerical answer.");
-            // event.preventDefault();
-        } else if (validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number") {
-            alert("Field requires letters. Please only enter letters or words.");
-            // event.preventDefault();
+
+        } else if (
+            (validateInput(fuelLevel) === "Not a Number" || 
+            validateInput(cargoLevel) === "Not a Number") &&
+            (validateInput(pilot) === "Is a Number" || 
+            validateInput(copilot) === "Is a Number")) {
+
+            alert("Names require letter inputs. Amounts require number inputs. Please enter the correct corresponding inputs.");
+        } else if (
+            validateInput(fuelLevel) === "Not a Number" || 
+            validateInput(cargoLevel) === "Not a Number") {
+                
+            alert("Amounts require number inputs. Please enter the correct corresponding inputs.");
+
+        } else if (
+            validateInput(pilot) === "Is a Number" || 
+            validateInput(copilot) === "Is a Number") {
+
+            alert("Names require letter inputs. Please enter the correct corresponding inputs.");
+
         } else {
-            return;
-        };
-
-        console.log("got to after conditionals for checking submit");
-
-
-        // document.querySelector(list);
-        // do i need to re-include list?
-
-        document.getElementById("pilotStatus").innerHTML = `Pilot ${pilot} is ready for launch`;
-        document.getElementById("copilotStatus").innerHTML = `Co-pilot ${copilot} is ready for launch`;
         
-        if (fuelLevel < 10000) {
-            document.getElementById("faultyItems").style.visibility = "visible";
-            document.getElementById("fuelStatus").innerHTML = "Fuel level too low for launch";
-            document.getElementById("launchStatus").innerHTML = "Shuttle Not Ready for launch";
-            document.getElementById("launchStatusCheck").style.color = "rgb(199, 37, 78)";
-        } 
-        if (cargoLevel > 10000) {
-            document.getElementById("faultyItems").style.visibility = "visible";
-            document.getElementById("cargoStatus").innerHTML = "Cargo mass too heavy for launch";
-            document.getElementById("launchStatus").innerHTML = "Shuttle Not Ready for Launch";
-            document.getElementById("launchStatusCheck").style.color = "rgb(199, 37, 78)";
-        } else {
-            document.getElementById("launchStatusCheck").style.color = "green";
-            document.getElementById("launchStatusCheck").innerHTML = "Shuttle is Ready for Launch";
-        }
-    // });
+            if (Number(fuelLevel) < 10000 && Number(cargoLevel) <= 10000) {
+
+                list.style.visibility = "visible";
+                document.getElementById("pilotStatus").innerHTML = `Pilot ${pilot} is ready for launch`;
+                document.getElementById("copilotStatus").innerHTML = `Co-pilot ${copilot} is ready for launch`;
+                document.getElementById("fuelStatus").innerHTML = "Fuel level too low for launch";
+                document.getElementById("launchStatus").innerHTML = "Shuttle Not Ready for launch";
+                document.getElementById("launchStatusCheck").style.color = "rgb(199, 37, 78)";
+
+            } else if (Number(cargoLevel) > 10000 && Number(fuelLevel) >= 10000) {
+
+                list.style.visibility = "visible";
+                document.getElementById("pilotStatus").innerHTML = `Pilot ${pilot} is ready for launch`;
+                document.getElementById("copilotStatus").innerHTML = `Co-pilot ${copilot} is ready for launch`;
+                document.getElementById("cargoStatus").innerHTML = "Cargo mass too heavy for launch";
+                document.getElementById("launchStatus").innerHTML = "Shuttle Not Ready for Launch";
+                document.getElementById("launchStatusCheck").style.color = "rgb(199, 37, 78)";
+
+            } else {
+
+                document.getElementById("launchStatusCheck").style.color = "green";
+                document.getElementById("launchStatusCheck").innerHTML = "Shuttle is Ready for Launch";
+            }
+        };
 };
+
 
 async function myFetch() {
     let planetsReturned;
